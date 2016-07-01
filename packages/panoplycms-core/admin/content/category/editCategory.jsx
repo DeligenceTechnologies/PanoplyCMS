740f28fd43643cf87bb2145de3f631979348fb7e
shakshi_deligence <shakshi.deligence@gmail.com>
2016-07-01 16:05:51 +0530

    List of categories,add category,edit catgory pages fixed with popup and sucess message and error message functionaliy.

--- packages/panoplycms-core/admin/content/category/editCategory.jsx
+++ packages/panoplycms-core/admin/content/category/editCategory.jsx
@@ -10,42 +10,58 @@ EditCategory = React.createClass({
   submitData(event){
     event.preventDefault();
     var title=ReactDOM.findDOMNode(this.refs.title).value.trim();
-    var alias=ReactDOM.findDOMNode(this.refs.alias).value.trim();
-    Meteor.call('update_category',this.props._id,title,alias,function(err,data){
+    var alias = generateAlias(title);
+    Meteor.call('update_category',this.props._id,title,alias,(err,data)=>{
         if(err)
-          console.log(err);
+          this.setState({errorMsg:err})
         else{
-          console.log(data);
-          FlowRouter.go('listCategories');      
+          this.setState({successMsg:true})  
         }
       });
   },
-
+  getInitialState(){
+    return{
+      errorMsg:false,
+      successMsg:false
+    }
+  },
+  resetSuccessMsg(){
+    this.setState({successMsg:false});
+    this.setState({errorMsg:false})
+  },
   render:function () {
 
     if (this.data.pageLoading) {
       return <LoadingSpinner />;
     }
+    if(this.state.successMsg){
+      msg=<AlertMessage data={'Successfully updated Category.'} func={this.resetSuccessMsg}/>
+    }else if(this.state.errorMsg){
+      msg=<AlertMessageOfError data={this.state.errorMsg} func={this.resetSuccessMsg}/>
+    }else{
+      msg=''
+    }
 
     return (
-       <div className="col-md-10 content">
+       <div className="col-md-10 content" onClick={this.resetSuccessMsg}>
        <Heading  data= {i18n('ADMIN_COTNENTS_CATEGORY_EDITCATEGORY')} />
-            <form className = "form-horizontal" role = "form" onSubmit={this.submitData}>
-                <div className="form-group">
-                  <label className="col-sm-2 control-label">{i18n('ADMIN_COTNENTS_CATEGORY_ADDCATEGORY_FORM_CATEGORYNAME')}</label>
-                  <div className="col-sm-10">
-                    <input type = "text" ref="title" id="title"  onChange={this.handleChange} className="form-control" defaultValue={this.data.categoryData.title}  placeholder = "Enter title" required />
-                  </div>
-                </div>
-                <div className="form-group">
-                  <div className="col-sm-offset-2 col-sm-10">
-                    <input type = "submit" className="btn btn-success" value={i18n('ADMIN_COTNENTS_CATEGORY_ADDCATEGORY_FORM_SAVE')} />
-                    &nbsp;&nbsp;
-                    <a href={FlowRouter.path('listCategories')} className="btn btn-danger">{i18n('ADMIN_COTNENTS_CATEGORY_ADDCATEGORY_FORM_CANCEL')}</a>
-                  </div>
-                </div>
-            </form>
-        </div>
+       {msg}
+        <form className = "form-horizontal" role = "form" onSubmit={this.submitData}>
+            <div className="form-group">
+              <label className="col-sm-2 control-label">{i18n('ADMIN_COTNENTS_CATEGORY_ADDCATEGORY_FORM_CATEGORYNAME')}</label>
+              <div className="col-sm-10">
+                <input type = "text" ref="title" id="title"  onChange={this.handleChange} className="form-control" defaultValue={this.data.categoryData.title}  placeholder = "Enter title" required />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-offset-2 col-sm-10">
+                <input type = "submit" className="btn btn-primary" value='UPDATE' />
+                &nbsp;&nbsp;
+                <a href={FlowRouter.path('listCategories')} className="btn btn-danger">CANCEL</a>
+              </div>
+            </div>
+        </form>
+    </div>
     )
   }
 });
--- packages/panoplycms-core/admin/content/category/listCategories.jsx
+++ packages/panoplycms-core/admin/content/category/listCategories.jsx
@@ -16,34 +16,35 @@ ListCategories = React.createClass({
              <Heading  data={i18n('ADMIN_COTNENTS_CATEGORY_CATEGORY')} />
               <div className="panel-heading">
                 <a href={FlowRouter.path('addCategory')} className="btn btn-success btn-ico">
-                    <i className="fa fa-plus-circle "></i>
-                    {i18n('ADMIN_COTNENTS_CATEGORY_ADDCATEGORY')}
+                    <i className="fa fa-plus-circle fa-lg"></i>&nbsp;
+                       {i18n('ADMIN_COTNENTS_CATEGORY_ADDCATEGORY')}
                 </a>
               </div>
-              <div className="panel-body">
-                
-                  <div className="table-responsive" id="non-editable">
-                    <table className="table  table-bordered">
-                      <thead>
-                        <tr>
-                          <th>{i18n('ADMIN_COTNENTS_CATEGORY_ADDCATEGORY_FORM_CATEGORYNAME')}</th>
-                          <th>Action</th>
-                          <th>Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {this.data.Categories.map(function(cat){
-                          return <CategoriesItem key={cat._id} data={cat} />
-                        })
-                      }
-                         
-                      </tbody>
-                    </table>
-                  </div>
+              <div className="panel-body"> 
+                <div className="table-responsive" id="non-editable">
+                  <table className="table  table-bordered">
+                    <thead>
+                      <tr>
+                        <th>{i18n('ADMIN_COTNENTS_CATEGORY_ADDCATEGORY_FORM_CATEGORYNAME')}</th>
+                        <th>Action</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.data.Categories.map(function(cat){
+                        return <CategoriesItem key={cat._id} data={cat} />
+                      })
+                    }   
+                    </tbody>
+                  </table>
                 </div>
-              </div> 
-           
-    </div>
+              </div>
+              {this.data.Categories.map(function(cat){
+                return  <ModalOfCat key={cat._id} data={cat}/> 
+                })
+              }
+            </div>     
+          </div>
     );
   }
 });
@@ -61,10 +62,55 @@ var CategoriesItem = React.createClass({
     render(){
       return(                                      
         <tr>
-          <td><a href={FlowRouter.path('editCategory',{_id:this.props.data._id})} ><large> {this.props.data.title}</large><small> (<em>Alias:{this.props.data.alias}</em> )</small> </a></td>
-          <td><div  onClick={this.deleteCategory} className="delete_btn"><i className="fa fa-trash-o" data-toggle="tooltip" title="Delete" ></i> </div></td>
-          <td><div  onClick={this.editCategory} className="edit_btn"  id=""><a href={FlowRouter.path('editCategory',{_id:this.props.data._id})}><i className="fa fa-pencil-square-o" data-toggle="tooltip" title="Edit"></i></a> </div></td>
+          <td>
+            <a href={FlowRouter.path('editCategory',{_id:this.props.data._id})} >
+              <large> 
+                {this.props.data.title}
+              </large>
+              <small> 
+                (<em>Alias:{this.props.data.alias}</em> )
+              </small> 
+            </a>
+          </td>
+          <td>
+            <div className="delete_btn" data-toggle="modal" data-target={"#"+this.props.data._id}>
+              <i className="fa fa-trash-o" data-toggle="tooltip" title="Delete" ></i> 
+            </div>
+          </td>
+          <td>
+            <div  onClick={this.editCategory} className="edit_btn"  id="">
+              <a href={FlowRouter.path('editCategory',{_id:this.props.data._id})}>
+                <i className="fa fa-pencil-square-o" data-toggle="tooltip" title="Edit"></i>
+              </a>
+            </div>
+          </td>
         </tr>    
       )
     }
+})
+
+ModalOfCat=React.createClass({
+  deleteCategory(){
+    Meteor.call('delete_category',this.props.data._id,function(err,data){
+      console.log(err,data)
+    });
+  },
+  render:function(){
+    return(
+          <div id={this.props.data._id} className="modal fade" role="dialog">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-body">
+                  <button type="button" className="close" data-dismiss="modal">&times;</button>
+                  <h4 className="modal-title">Do you really want to remove ?</h4>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-primary" onClick={this.deleteCategory} data-dismiss="modal">YES</button>
+                  <button type="button" className="btn btn-danger" data-dismiss="modal">NO</button>
+                </div>
+              </div>
+            </div>
+          </div>
+    )     
+  }
 })

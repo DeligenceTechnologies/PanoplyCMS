79a0eb0878e6cf8b4a28efb3d875a305e223d359
Praful <praful@deligence.com>
2016-07-13 15:57:48 +0530

    Default Template Footer and css corrected

--- packages/default-template/client/homeLayouts/Header.jsx
+++ packages/default-template/client/homeLayouts/Header.jsx
@@ -2,7 +2,7 @@ FrontHeader = (data) => {
 	return(
 		<div className="blog-masthead">
 			<div className="container">
-				<nav className="nav blog-nav">
+				<nav className="nav blog-nav" id="main-nav">
 					{data.module?data.module.map(m => {
 						return m
 					}):''}

9b799f79806486a68a579b0a2b6bc217fed033d3
Saransh Dev Singh <saransh@deligence.com>
2017-05-20 13:42:12 +0530

    New Version - v1.3
    New Version - v1.3

--- packages/default-template/server/startup.js
+++ packages/default-template/server/startup.js
@@ -1,6 +1,4 @@
 Meteor.startup(function() {
-	//	FormHandler.ignoreFields = ["_id", "createdAt", "updatedAt"];
-
 	PanoplyCMSRegisterPackage({
 		"name" : "template",
 		"templates": {
@@ -8,12 +6,32 @@ Meteor.startup(function() {
 			packageName: "deligencetechnologies:default-template",
 			layoutImage: "public/templateLayout.jpg",
 			active: true,
-			layout: "DefaultTemplate",
-			notFound:'DefaultNotFound',
-			offline:'DefaultOffline',
-			categoryView: "DefaultCategory",
-			articleView: "DefaultArticle",
-			positions: ['top', 'sidebar', 'footer']
+			layout: "DefaultTemplate",			
+			views:{
+				notFound:'DefaultNotFound',
+				offline:'DefaultOffline',
+				category: "DefaultCategory",
+				article: "DefaultArticle",
+				module: "ModuleOnly"
+			},
+			positions: [
+				'topHeader',
+				'mainHeader',
+				'showcase',
+				'utility',
+				'feature',
+				'mainTop',
+				'sidebar',
+				'contentTop',
+				'mainbody',
+				'contentBottom',
+				'mainBottom',
+				'extension',
+				'fullWidth',
+				'bottom',
+				'mainFooter',
+				'copyright'
+			]
 		}
 	})
 });

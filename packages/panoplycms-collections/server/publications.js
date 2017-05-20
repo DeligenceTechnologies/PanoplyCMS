be33f1bd4b3ac5efede61b1384a5886b439d0be9
Saransh Dev Singh <saransh@deligence.com>
2017-05-20 13:49:26 +0530

    New Version - v1.3
    New Version - v1.3

--- packages/panoplycms-collections/server/publications.js
+++ packages/panoplycms-collections/server/publications.js
@@ -6,6 +6,12 @@ Meteor.publish('tags', function(){
 	return PanoplyCMSCollections.Tags.find({});
 });
 
+Meteor.publish('tagsLimit', function(limit){
+	if(limit){
+		return PanoplyCMSCollections.Tags.find({}, {limit:limit+1});
+	}
+});
+
 Meteor.publish('findTag', function(id){	
 	return PanoplyCMSCollections.Tags.find({_id:id});
 });
@@ -33,6 +39,11 @@ Meteor.publish('articlesFind', function(){
 Meteor.publish('findOneArticle', function(id){
 	return PanoplyCMSCollections.Articles.find({_id:id});
 });
+Meteor.publish('articles', function(limit){
+	if(limit){
+		return PanoplyCMSCollections.Articles.find({}, {limit: limit + 1});
+	}
+});
 
 /* Publish Categories */
 Meteor.publish('Categories', function(){
@@ -41,6 +52,17 @@ Meteor.publish('Categories', function(){
 Meteor.publish('findCategory', function(id){
 	return PanoplyCMSCollections.Categories.find({_id:id});
 });
+Meteor.publish('category', function(limit){
+	if(limit){
+		return PanoplyCMSCollections.Categories.find({trash: false}, {limit:limit+1});
+	}
+});
+
+Meteor.publish('categoryTrash', function(limit){
+	if(limit){
+		return PanoplyCMSCollections.Categories.find({trash: true}, {limit:limit+1});
+	}
+});
 
 /* Publish Modules */
 Meteor.publish('moduleList', function(){
@@ -48,4 +70,17 @@ Meteor.publish('moduleList', function(){
 });
 Meteor.publish('findAModule', function(id){
 	return PanoplyCMSCollections.Modules.find({_id:id});
+});
+
+Meteor.publish('modules', function(limit){
+	// console.log("current page ------", page)
+	// console.log("page limit ------", limit)
+	// if(page && limit){
+	if(limit){
+		// let skip = (page - 1)*limit;
+		// console.log("page skip ------", skip)
+		// console.log("Data >>>>", PanoplyCMSCollections.Modules.find({}, {skip: skip, limit: limit}).fetch())
+		// return PanoplyCMSCollections.Modules.find({}, {skip: skip, limit: limit});
+		return PanoplyCMSCollections.Modules.find({}, {limit: limit + 1});
+	}
 });
